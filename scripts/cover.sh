9affa89fd876e7c675dbe33bdbdf328ca3531198
Joshua T Corbin <joshua@uber.com>
2018-06-20 10:28:29 -0700

    Try -coverpkg=all instad of a new test suite

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -3,8 +3,8 @@
 set -e
 echo "" > cover.out
 
-for d in $(go list $@); do
-    go test -race -coverprofile=profile.out $d
+for d in $(go list "$@"); do
+    go test -race -coverprofile=profile.out -coverpkg=all "$d"
     if [ -f profile.out ]; then
         cat profile.out >> cover.out
         rm profile.out
